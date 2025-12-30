ce63a3ffe71d5f75db9285977bc548f04dc2abc3
Ronaldo <ronald.rosa96@gmail.com>
2025-12-29 21:59:06 -0500

    primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="MIREPO3",                           # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="RONALDO ROSADO",                         # Tu nombre
+    author_email="ronald.rosa96@gmail.com",                 # Tu correo electrónico
+    url="https://github.com/Economiststata/MIREPO3",     # URL del proyecto
+)
